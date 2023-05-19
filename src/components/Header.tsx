378f22d5ee10c5407957d701f21b3f08e5f46ba7
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-20 00:07:08 +0100

    Basic hiding all nav links when on smaller displays

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,43 +15,48 @@ export default function Header() {
 
   return (
     <header className='flex justify-between px-8 gap-8 items-center bg-white py-4'>
-      <TbListSearch className='sm:hidden' />
+      <button className='md:hidden'>
+        <TbListSearch className='header-icon' />
+      </button>
 
       <Link to='/' className='font-oswald font-extrabold text-2xl'>
         TeeFusion
       </Link>
 
-      <nav className='flex justify-around grow items-center'>
+      <nav className='justify-around grow items-center hidden md:flex'>
         <div className='flex gap-8'>
           <HeaderLink to='/shop/women' text='Women' />
           <HeaderLink to='/shop/men' text='Men' />
         </div>
 
-        <form className='border bg-gray-200 flex p-1 rounded focus-within:outline outline-black outline-2'>
+        <form className='border bg-gray-200 flex rounded-sm focus-within:outline outline-black outline-2'>
           <label htmlFor='search'>
             <span className='hidden'>Search Product</span>
             <input
               type='text'
               name='search'
               id='search'
-              className='bg-gray-200 focus:outline-none'
+              className='bg-gray-200 focus:outline-none p-2 pr-0'
             />
           </label>
 
-          <button type='submit'>
+          <button type='submit' className='pr-2'>
             <AiOutlineSearch className='header-icon' />
           </button>
         </form>
 
-        <button className='not-built'>Sign In/ Register</button>
+        <button className='not-built hover:font-semibold w-32'>
+          Sign In/ Register
+        </button>
       </nav>
 
       <nav className='flex gap-4'>
         <button className='not-built' aria-label='Wishlist'>
           <AiOutlineHeart className='header-icon' />
         </button>
-        <button aria-label='Basket'>
+        <button aria-label='Basket' className='flex gap-1 items-center'>
           <AiOutlineShopping className='header-icon' />
+          <p>0</p>
         </button>
       </nav>
     </header>
--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function HeaderLink({ to, text }: { to: string; text: string }) {
   return (
-    <Link to={to} className=''>
+    <Link to={to} className='hover:font-semibold w-16'>
       {text}
     </Link>
   );
