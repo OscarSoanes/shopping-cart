3bedd90fb6be89f2235c57d2a091ccf4c7b202a1
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-20 16:59:35 +0100

    Footer added and styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,90 @@
+import {
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+} from "react-icons/ai";
+import { BsTiktok } from "react-icons/bs";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className='flex flex-col p-8 gap-16 md:flex-row'>
+      <section className='flex-1 grid justify-items-center gap-8 text-center h-max'>
+        <p>
+          Contact our dedicated customer support team for any inquiries or
+          assistance. We're here to ensure your TeeFusion experience is nothing
+          short of exceptional.
+        </p>
+
+        <SocialIcons />
+      </section>
+      <section className='flex-1 flex gap-8 justify-between'>
+        <div className='flex-1'>
+          <h2 className='text-xl mb-4'>Help & Support</h2>
+          <SmallLink
+            locations={[
+              "FAQ",
+              "Delivery",
+              "Returns & Refunds",
+              "Orders & Payment",
+              "Size Guide",
+              "Store Locator",
+            ]}
+          />
+        </div>
+
+        <div className='flex-1'>
+          <h2 className='text-xl mb-4'>About Us</h2>
+          <SmallLink
+            locations={[
+              "About TeeFusion",
+              "Company Information",
+              "Counterfeit Goods",
+              "Privacy Commitment",
+              "Privacy Notice",
+              "Cookie Notice",
+              "Terms & Conditions",
+            ]}
+          />
+        </div>
+      </section>
     </footer>
-  )
+  );
+}
+
+function SocialIcons() {
+  const icons = [
+    { icon: <AiOutlineInstagram />, href: "https://www.instagram.com/" },
+    { icon: <AiOutlineTwitter />, href: "http://twitter.com/" },
+    { icon: <AiFillFacebook />, href: "https://www.facebook.com/" },
+    { icon: <BsTiktok />, href: "https://www.tiktok.com/en/" },
+  ];
+
+  return (
+    <nav className='flex gap-8'>
+      {icons.map((icon, index) => (
+        <a
+          key={index}
+          href={icon.href}
+          className='text-2xl bg-black text-white p-2 rounded-sm hover:bg-gray-700 h-max'
+        >
+          {icon.icon}
+        </a>
+      ))}
+    </nav>
+  );
+}
+
+function SmallLink({ locations }: { locations: Array<String> }) {
+  return (
+    <ul className='flex flex-col gap-4'>
+      {locations.map((location, index) => (
+        <li
+          key={index}
+          className='cursor-not-allowed bg-gray-50 p-1 rounded hover:bg-gray-100 w-48'
+        >
+          {location}
+        </li>
+      ))}
+    </ul>
+  );
 }
