279a293b0a8d5d35ea9a4d826713b261fcfefcde
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-20 15:15:45 +0100

    Made the Buttons link to a location

--- src/components/HomeImageContainer.tsx
+++ src/components/HomeImageContainer.tsx
@@ -9,7 +9,7 @@ export function HomeImageContainer({ src, alt }: { src: string; alt: string }) {
         className='w-[50vw] object-cover object-top h-full'
       />
       <Link
-        to=''
+        to={`${alt === "Shop Women" ? "/shop/women" : "/shop/men"}`}
         className={`
         ${alt === "Shop Women" ? "bottom-16" : "top-16"}
         font-light absolute text-center mx-8 p-2 px-8 m-auto h-max bg-white text-2xl hover:bg-black hover:text-white inset-0 sm:bottom-0 sm:top-0 sm:w-max sm:mx-auto sm:py-4`}
