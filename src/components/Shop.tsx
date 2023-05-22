5db361200f6f76484135bd71b64d56105e42a5ce
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-22 17:41:16 +0100

    Printing the products name to page

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,14 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllByGender } from "../functions/api";
 
 export function Shop({ type }: { type: string }) {
+  const [data, setData] = useState<Array<object>>([]);
+
   useEffect(() => {
-    console.log(getAllByGender(type));
+    setData(getAllByGender(type));
   }, [type]);
 
   return (
     <main>
       <h1>Shop Page!! {type}</h1>
+
+      {data.length > 0 ? (
+        data.map((product: any) => <p key={product.id}>{product.name}</p>)
+      ) : (
+        <p>Loading...</p>
+      )}
     </main>
   );
 }
