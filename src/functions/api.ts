48c1539de2319efc68ab9cca4333fd2103806a58
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 13:36:01 +0100

    added a get by id function to api

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -7,3 +7,7 @@ export function getAllByGender(gender: string) {
 export function getAllBySearch(query: string) {
   return products.filter((product) => product.name.toLowerCase().includes(query.toLowerCase()) || product.brand.toLowerCase().includes(query.toLowerCase()))
 }
+
+export function getById(id: Number) {
+  return products.find((product) => product.id === id)
+}
