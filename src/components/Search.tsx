8394c37325f69850e769e7a8c7668dfd5a6c6e8e
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 13:13:07 +0100

    Search page built using a similar style to Shop

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,3 +1,24 @@
+import { useParams } from "react-router-dom";
+import { getAllBySearch } from "../functions/api";
+import { ProductGrid } from "./ProductGrid";
+
 export function Search() {
-  return <></>;
+  const params = useParams();
+  const query = params["*"]!;
+
+  console.log(query);
+  const data = getAllBySearch(query);
+
+  console.log(data);
+
+  return (
+    <main className='pt-4 p-8'>
+      <div className='flex justify-between py-4 pr-4 sm:pr-8 items-center'>
+        <h2 className='text-xl'>Results for: {query}</h2>
+        <p className='text-sm text-gray-700'>{data.length} Items</p>
+      </div>
+
+      <ProductGrid data={data} />
+    </main>
+  );
 }
