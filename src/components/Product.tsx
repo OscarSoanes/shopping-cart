460232d208603333dbca0bfeda7684f2a0068f12
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 15:04:14 +0100

    Loading product images in

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,64 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { getById } from "../functions/api";
+import { useEffect, useState } from "react";
 
 export function Product() {
   const params = useParams();
-
+  const navigate = useNavigate();
   const id = params["*"]!;
 
-  const data = getById(parseInt(id));
+  const [data, setData] = useState(Object);
+  const [image, setImage] = useState("");
+  const [alternateImages, setAlternateImages] = useState(Array<string>);
 
-  return <main>{data && <p>{data.name}</p>}</main>;
+  useEffect(() => {
+    const data = getById(parseInt(id));
+    if (!data) {
+      navigate("/404");
+      return;
+    }
+    setData(data);
+  }, [id, navigate]);
+
+  useEffect(() => {
+    if (!data.image) {
+      return;
+    }
+
+    // Setting Images
+    import(`../media/${data.image}`).then((image) => {
+      setImage(image.default);
+    });
+
+    data["alternate-images"].forEach((nextImage: string) => {
+      import(`../media/${nextImage}`).then((alternate) => {
+        setAlternateImages((prevImages) => {
+          const toSet = [...prevImages, alternate.default];
+          return toSet;
+        });
+      });
+    });
+  }, [data]);
+
+  return (
+    data.name && (
+      <main>
+        <p>
+          <Link to='/'>Home</Link> -{" "}
+          <Link to={`/shop/${data.for}`}>
+            {data.for.charAt(0).toUpperCase()}
+            {data.for.substring(1)}
+          </Link>
+        </p>
+
+        <img src={image} alt={`1. ${data.name} ${data.brand}`} />
+        {alternateImages.map((alternate, index) => (
+          <img
+            src={alternate}
+            alt={`${index + 2}. ${data.name} ${data.brand}`}
+          />
+        ))}
+      </main>
+    )
+  );
 }
