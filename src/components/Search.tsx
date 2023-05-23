ac38b265ba13dcefc271265f833a38abbc4b5b61
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 13:15:15 +0100

    Prettified the search query

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,7 +14,10 @@ export function Search() {
   return (
     <main className='pt-4 p-8'>
       <div className='flex justify-between py-4 pr-4 sm:pr-8 items-center'>
-        <h2 className='text-xl'>Results for: {query}</h2>
+        <h2 className='text-xl'>
+          Results for:{" "}
+          {query.charAt(0).toUpperCase() + query.substring(1).toLowerCase()}
+        </h2>
         <p className='text-sm text-gray-700'>{data.length} Items</p>
       </div>
 
