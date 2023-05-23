e08ab6df8398eeb2749858b2e08cc8d17fb2b35a
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 16:28:56 +0100

    Added images to be displayed on a mobile screen

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,14 +2,15 @@ import { useParams, useNavigate, Link } from "react-router-dom";
 import { getById } from "../functions/api";
 import { useEffect, useState } from "react";
 
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+
 export function Product() {
   const params = useParams();
   const navigate = useNavigate();
   const id = params["*"]!;
 
   const [data, setData] = useState(Object);
-  const [image, setImage] = useState("");
-  const [alternateImages, setAlternateImages] = useState(Array<string>);
+  const [images, setImages] = useState(Array<string>);
 
   useEffect(() => {
     const data = getById(parseInt(id));
@@ -27,12 +28,12 @@ export function Product() {
 
     // Setting Images
     import(`../media/${data.image}`).then((image) => {
-      setImage(image.default);
+      setImages([image.default]);
     });
 
     data["alternate-images"].forEach((nextImage: string) => {
       import(`../media/${nextImage}`).then((alternate) => {
-        setAlternateImages((prevImages) => {
+        setImages((prevImages) => {
           const toSet = [...prevImages, alternate.default];
           return toSet;
         });
@@ -42,22 +43,31 @@ export function Product() {
 
   return (
     data.name && (
-      <main>
-        <p>
-          <Link to='/'>Home</Link> -{" "}
-          <Link to={`/shop/${data.for}`}>
+      <main className='md:py-4 md:px-8'>
+        <p className='flex gap-4 py-4 px-8 md:px-0 md:py-2 text-gray-600'>
+          <Link to='/' className='hover:text-gray-700'>
+            Home
+          </Link>{" "}
+          -{" "}
+          <Link to={`/shop/${data.for}`} className='hover:text-gray-700'>
             {data.for.charAt(0).toUpperCase()}
             {data.for.substring(1)}
           </Link>
         </p>
 
-        <img src={image} alt={`1. ${data.name} ${data.brand}`} />
-        {alternateImages.map((alternate, index) => (
-          <img
-            src={alternate}
-            alt={`${index + 2}. ${data.name} ${data.brand}`}
-          />
-        ))}
+        <div className='grid md:grid-cols-3 gap-8'>
+          <section className='flex md:col-span-2 md:grid images gap-2 overflow-x-auto overflow-ellipsis snap-x snap-mandatory'>
+            {images.map((image, index) => (
+              <img
+                src={image}
+                alt={`${index + 1}. ${data.name} ${data.brand}`}
+                className='snap-center w-full'
+              />
+            ))}
+          </section>
+
+          <section></section>
+        </div>
       </main>
     )
   );
