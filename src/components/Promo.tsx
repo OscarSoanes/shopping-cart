50b91d011e3381e25fc2e105fca3b3b7bc44b21a
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-23 17:39:13 +0100

    Used underline on promo

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -1,7 +1,7 @@
 export function Promo() {
   return (
     <header className='flex justify-center bg-black text-white p-2 px-6 text-center'>
-      <p className='border-b-2 border-black hover:border-white cursor-not-allowed'>
+      <p className='border-b-2 border-black hover:underline cursor-not-allowed'>
         Free delivery on all orders over £30 + 90-day free returns
       </p>
     </header>
