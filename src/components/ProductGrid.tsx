8f64a248869131e6af9ec3094b898edb3953b037
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-24 20:05:15 +0100

    Fixed bug where search would be loading forever if no results

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -4,9 +4,7 @@ export function ProductGrid({ data }: { data: Array<object> }) {
   return (
     <div className='grid products gap-8'>
       {data.length > 0 ? (
-        data.map((product: any) => (
-          <ProductContainer key={product.id} product={product} />
-        ))
+        data.map((product: any) => <ProductContainer key={product.id} product={product} />)
       ) : (
         <p className='text-center text-xl'>Loading...</p>
       )}
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -5,23 +5,18 @@ import { ProductGrid } from "./ProductGrid";
 export function Search() {
   const params = useParams();
   const query = params["*"]!;
-
-  console.log(query);
   const data = getAllBySearch(query);
-
-  console.log(data);
 
   return (
     <main className='pt-4 p-8'>
       <div className='flex justify-between py-4 pr-4 sm:pr-8 items-center'>
         <h2 className='text-xl'>
-          Results for:{" "}
-          {query.charAt(0).toUpperCase() + query.substring(1).toLowerCase()}
+          Results for: {query.charAt(0).toUpperCase() + query.substring(1).toLowerCase()}
         </h2>
         <p className='text-sm text-gray-700'>{data.length} Items</p>
       </div>
 
-      <ProductGrid data={data} />
+      {data.length === 0 ? <p>No results found for {query}</p> : <ProductGrid data={data} />}
     </main>
   );
 }
